69f6f6de0dd5005321632342dee9eae141bd3f3d
Claaarree <clare.fh@hotmail.com>
2024-11-25 21:23:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Task
+
+1. Read the products text file.
+2. Load into a collection of products.
+3. Display in a table on a html document.
+4. Include a "buy" button such that clicking it would increment sold amount and decrease stock left.
+5. Save the updated product collection so that it would retain information when loaded up next time.
---
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_day13hw/Vttp5aSsfDay13hwApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_ssf_day13hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aSsfDay13hwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aSsfDay13hwApplication.class, args);
+	}
+
+}
